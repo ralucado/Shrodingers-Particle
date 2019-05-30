72e5a2cd69df00a0f1a26cb04b91f06bcfda78f1
Ralu <ralucris.v@gmail.com>
2019-05-30 12:22:19 +0200

    minor improvements

--- prac5.m
+++ prac5.m
@@ -3,7 +3,7 @@ list = {'Electron','Neutron','Proton','Hydrogen','Neon','Other'};
 [indx,tf] = listdlg('PromptString','Select a mass:',...
                            'SelectionMode','single',...
                            'ListString',list);
-masses = {9.11*10^-34, 1.674929*10^-30, 3.47053*10^-30, 1.6735575*10^-30, 3.350920307*10^-25};
+masses = {9.109*10^-28, 1.674929*10^-24, 1.672621*10^-24, 1.6737*10^-24, 3.350920307*10^-23};
 if indx == 6
     m = inputdlg('Insert "m" value (in g):');
     m = str2double(m{1});
@@ -18,11 +18,17 @@ definput = {'100e-12'};
 A = inputdlg(prompt,dlgtitle,[1 40],definput);
 a = str2double(A{1:1});
 
-n = inputdlg('Insert "n" value:');
-n = str2double(n{1});
+prompt = {'Insert "n" value:'};
+dlgtitle = 'n';
+definput = {'3'};
+N = inputdlg(prompt,dlgtitle,[1 40],definput);
+n = str2double(N{1:1});
 
-X = inputdlg('Insert specific x value between -a/2 and a/2:');
-X = str2double(X{1});
+prompt = {'Insert specific x value between -a/2 and a/2:'};
+dlgtitle = 'X';
+definput = {'0'};
+x = inputdlg(prompt,dlgtitle,[1 40],definput);
+x = str2double(x{1:1});
 
 
 global h;
@@ -33,12 +39,12 @@ fig = figure();
 subplot(4, 2, [1 3]);
 density = @(x) densitat(n,a,x);
 fplot(density, [-a/2 a/2])
-title('Probabilitat');
+title('Prob. Density');
 
 subplot(4, 2, [2 4]);
 Y = arrayfun(@(x) energia(x,m,a),0:n*2);
 stairs(Y)
-title("Nivells d'energia");
+title("Energy Levels");
 
 %fM = figure();
 %mom = @(x) moment(x,a);
@@ -47,7 +53,7 @@ title("Nivells d'energia");
 subplot(4, 2, [6 8]);
 wave = @(x) funciodona(n,a,x);
 fplot(wave, [-a/2 a/2])
-title("Funció d'ona");
+title("Wave Function");
 
 caption = sprintf('\nProbability on point %g = %.3g\n\nEnergy on level %d = %.3g J\n\nLineal momentum on level %d = %.3g N*s', X, densitat(n,a,X),  n, energia(n,a,m), n, moment(n,a));
 ax = subplot(4, 2, 5);
